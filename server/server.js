6fbacdb90fc526437d0741b0db8dca379decb3b1
Nagaraj M R <nagaraj.muktenahalli@agilesphere.com>
2018-05-13 15:25:41 +0100

    Revert: nmr: added express test

--- server/server.js
+++ server/server.js
@@ -3,8 +3,6 @@ const express = require('express');
 const app = express();
 
 app.get('/info', (req, res) => {
-    //added comment
-
     res.status(200).send({
         name: "Nagaraj",
         age: 27
